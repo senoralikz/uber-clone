ba2abd9fc9b04192c42fa1fb66fd0f09d5059c03
senoralikz <alexjm825@gmail.com>
2022-09-05 17:54:59 -0400

    added rides button in navigate card component

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,11 +1,62 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 import React from "react";
+import tw from "twrnc";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+
+const data = [
+  {
+    id: "Uber-X-123",
+    title: "UberX",
+    multiplier: 1,
+    image: "https://links.papareact.com/3pn",
+  },
+  {
+    id: "Uber-XL-456",
+    title: "Uber XL",
+    multiplier: 1.2,
+    image: "https://links.papareact.com/5w8",
+  },
+  {
+    id: "Uber-Lux-789",
+    title: "Uber LUX",
+    multiplier: 1.75,
+    image: "https://links.papareact.com/7pf",
+  },
+];
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>RideOptionsCard</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("NavigateCard")}
+          style={tw`absolute top-3 p-3 rounded-full`}
+        >
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+      </View>
+
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <Text>Car</Text>
+          </TouchableOpacity>
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
